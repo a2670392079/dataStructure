57b0accddcd4abff8f902053cbdd503be004080c
a2670392079 <37444340+a2670392079@users.noreply.github.com>
2021-09-18 18:30:55 +0800

    docs: 添加注释

--- core/PriorityQueue.ts
+++ core/PriorityQueue.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-09-18 10:55:59
+ * @LastEditTime: 2021-09-18 16:59:18
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \dataStructure\core\PriorityQueue.ts
+ */
 export default class PriorityQueue {
   private heap: Array<number>;
 
@@ -5,7 +13,16 @@ export default class PriorityQueue {
     this.heap = new Array();
   }
 
-  public static from(nums: Array<number>) {
+     
+  /**
+   * 从数组生成优先队列
+   *
+   * @static
+   * @param {Array<number>} nums
+   * @return {*}  {PriorityQueue}
+   * @memberof PriorityQueue
+   */
+  public static from(nums: Array<number>): PriorityQueue {
       const priorityQueue = new PriorityQueue();
       for(let i = 0; i < nums.length; i++){
           priorityQueue.enQueue(nums[i]);
@@ -14,25 +31,55 @@ export default class PriorityQueue {
       return priorityQueue
   }
 
-  public enQueue(num: number) {
+   
+  /**
+   * 入队,节点上浮
+   *
+   * @param {number} num
+   * @memberof PriorityQueue
+   */
+  public enQueue(num: number):void {
     this.heap.push(num);
     this.up(this.heap.length - 1);
   }
 
-  public deQueue() {
+    
+  /**
+   * 出队，节点下沉
+   *
+   * @return {*}  {number}
+   * @memberof PriorityQueue
+   */
+  public deQueue():number {
     const end = this.heap.shift();
     if (this.heap.length) {
       this.heap.unshift(this.heap.pop() as number);
       this.down(0, (this.heap.length - 2) >> 1);
     }
-    return end;
+    return end ?? 0;
   }
 
+   
+  /**
+   * 返回队列头部
+   *
+   * @return {*}  {number}
+   * @memberof PriorityQueue
+   */
   public frist(): number {
     return this.heap[0];
   }
 
-  private down(index: number, last_no_leaf_root_index: number) {
+   
+  /**
+   * 节点下沉方法
+   *
+   * @private
+   * @param {number} index
+   * @param {number} last_no_leaf_root_index
+   * @memberof PriorityQueue
+   */
+  private down(index: number, last_no_leaf_root_index: number):void {
     const heap = this.heap;
     if (index < last_no_leaf_root_index) {
       while (index <= last_no_leaf_root_index) {
@@ -51,7 +98,12 @@ export default class PriorityQueue {
     }
   }
 
-  private up(index: number) {
+  /**
+   * @private
+   * @param {number} index
+   * @memberof PriorityQueue
+   */
+  private up(index: number):void {
     const heap = this.heap;
     while (index !== 0) {
       const root = (index - 1) >> 1;
